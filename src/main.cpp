0c812732687388da64851dfd74f946efe757a065
Ivan Padilla <ivanlab@gmail.com>
2021-04-30 17:50:59 +0300

    MQTT working config

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,17 @@
  */
 
 #include "Particle.h"
-
 #include "tracker_config.h"
 #include "tracker.h"
+#include "MQTT.h"
 
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(SEMI_AUTOMATIC);
+#define SENSOR_NAME          "Nelium-Tracker-3"
 
 PRODUCT_ID(TRACKER_PRODUCT_ID);
 PRODUCT_VERSION(TRACKER_PRODUCT_VERSION);
+
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 STARTUP(
     Tracker::startup();
@@ -36,12 +38,100 @@ SerialLogHandler logHandler(115200, LOG_LEVEL_TRACE, {
     { "net.ppp.client", LOG_LEVEL_INFO },
 });
 
+void locationGenerationCallback(JSONWriter &writer, LocationPoint &point, const void *context); // Forward declaration
+int getLocation(LocationPoint& point);
+int getStatus(LocationStatus& status);
+void mqttPublishField();
+
+const char* broker = "mqtt.ivanlab.org";
+const char *topicData = "ivanlab/tracker/S/3";
+const char* topicCtrl = "ivanlab/tracker/C/3";
+const char* topicCtrlRx = "ivanlab/tracker/CC/3";
+
+int keepalive = 5;
+void callback(char* topic, byte* payload, unsigned int length);
+MQTT client("mqtt.ivanlab.org", 1883, keepalive, callback);
+
+unsigned long lastConnectionTime = 0; 
+const unsigned long postingInterval = 30L * 1000L; // Post data every 30 seconds.
+
+
 void setup()
 {
-    Tracker::instance().init();
+    TrackerConfiguration config;
+    config.enableIoCanPowerSleep(true)
+          .enableIoCanPower(true);
+
+    Tracker::instance().init(config);
+    Tracker::instance().location.regLocGenCallback(locationGenerationCallback);
+
+    Particle.connect();
+
+// MQTT initialization
+    String connect = String(SENSOR_NAME) + " is connected";
+
+    while (!client.isConnected()){
+    client.connect(SENSOR_NAME);
+    delay(1000);
+    Log.info("!");
+    }
+
+    const char* msgBuffer = connect.c_str();
+    Log.info(connect);
+    client.publish(topicCtrl, msgBuffer );
+    client.subscribe(topicCtrlRx);
+
 }
+
+//=================================
 
 void loop()
 {
     Tracker::instance().loop();
+
+   client.loop();   // Call the loop continuously to establish connection to the server.
+
+
+    if (millis() - lastConnectionTime > postingInterval) 
+    {
+        mqttPublishField(); // Use this function to publish to a single field
+        lastConnectionTime = millis();
+    }
+}
+
+//=================================
+
+void locationGenerationCallback(JSONWriter &writer, LocationPoint &point, const void *context){
+    
+}
+
+void mqttPublishField() {
+
+    LocationStatus locationStatus;
+    LocationPoint locationPoint;
+
+    Tracker::instance().locationService.getStatus(locationStatus);
+    Tracker::instance().locationService.getLocation(locationPoint);
+    Log.info("GPS lock=%d", locationStatus.locked);
+    Log.info("GPS Latitude=%f", locationPoint.latitude);
+    Log.info("GPS Longitude=%f",locationPoint.longitude);
+    Log.info("GPS speed=%f",locationPoint.speed);
+    
+    // Check connected to MQTT
+
+        Log.info("Trying to reconnect");
+        client.connect(SENSOR_NAME);
+        delay(1000);
+
+    // String data = String("GPS Latitude=%f", locationPoint.latitude);
+    String data = "{\"Lat\":\"" + String(locationPoint.latitude) + "\"}";
+    const char* msgBuffer = data.c_str();
+    Log.info(data);
+    client.publish(topicData, msgBuffer );
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
 }
